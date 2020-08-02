2ad5789eeebae042478f9899e2a3ebaf94791806
Michael Margolis <memargolis@gmail.com>
2020-08-02 07:50:46 +0100

    added missing file, updated paths for makegui batch file

--- runtime/Tools/makegui.bat
+++ runtime/Tools/makegui.bat
@@ -1,14 +1,15 @@
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 main_gui.ui -o main_gui.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 client\simple_input_gui.ui -o client\simple_input_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 coaster\coaster_gui.ui -o coaster\coaster_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 SpaceCoaster\space_coaster_gui.ui -o SpaceCoaster\space_coaster_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 SpaceCoaster\remote_client_gui.ui -o SpaceCoaster\remote_client_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 SpaceCoaster\local_client_gui.ui -o SpaceCoaster\local_client_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 common\dynamics_gui.ui -o common\dynamics_gui_defs.py
-rem call C:\Python27\Lib\site-packages\PyQt5\pyuic5 output\output_gui.ui -o output\output_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 output_gui.ui -o output\output_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 output\fstlib\festo_emulator.ui -o output\fstlib\festo_emulator_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 echo_handler_gui_defs.ui -o echo_handler_gui_defs.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 PlatformCalibrate\calibration_gui.ui -o PlatformCalibrate\calibration_gui_defs.py
-rem call C:\Python27\Lib\site-packages\PyQt5\pyuic5 client_select_dialog.ui -o client_select_dialog.py
-call C:\Python27\Lib\site-packages\PyQt5\pyuic5 gui_xform_test.ui -o gui_xform_test_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\main_gui.ui -o main_gui.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\test_client\simple_input_gui.ui -o ..\clients\test_client\simple_input_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\coaster\coaster_gui.ui -o ..\clients\coaster\coaster_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\SpaceCoaster\space_coaster_gui.ui -o ..\clients\SpaceCoaster\space_coaster_gui_defs.py
+rem call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\SpaceCoaster\remote_client_gui.ui -o ..\clients\SpaceCoaster\remote_client_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\remote_client\remote_client_gui.ui -o ..\clients\remote_client\remote_client_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\clients\SpaceCoaster\local_client_gui.ui -o ..\clients\SpaceCoaster\local_client_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\common\dynamics_gui.ui -o ..\common\dynamics_gui_defs.py
+rem call C:\Python27\Lib\site-packages\PyQt5\pyuic5 output\output_gui.ui -o ..\output\output_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\output_gui.ui -o ..\output\output_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\output\fstlib\festo_emulator.ui -o ..\output\fstlib\festo_emulator_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\echo_handler_gui_defs.ui -o ..\echo_handler_gui_defs.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\PlatformCalibrate\calibration_gui.ui -o ..\PlatformCalibrate\calibration_gui_defs.py
+rem call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\client_select_dialog.ui -o..\client_select_dialog.py
+call C:\Python27\Lib\site-packages\PyQt5\pyuic5 ..\gui_xform_test.ui -o ..\gui_xform_test_defs.py
