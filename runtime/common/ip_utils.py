87c965db3a619dae18f30bb9c69ad396127ff2a4
michaelmargolis <memargolis@gmail.com>
2022-02-04 17:16:40 +0000

    initial commit of ip_utils module

---
+++ runtime/common/ip_utils.py
@@ -0,0 +1,31 @@
+# ip_utils.py
+
+import socket
+import subprocess # for ping
+import platform
+
+
+def ping_ip(current_ip_address):
+    try:
+        output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower(
+        ) == "windows" else 'c', current_ip_address ), shell=True, universal_newlines=True)
+        if 'unreachable' in output:
+            return False
+        else:
+            return True
+    except Exception:
+            return False
+
+def get_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
